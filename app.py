bf7b62c3c4a92e105af8038f6f08dba10ef7a80c
Alan Nouman <tsoloevalan@gmail.com>
2025-08-29 16:45:25 +0300

    add delete_webhook

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 from aiogram import Bot, Dispatcher, types
 import asyncio
+from aiogram.filters import CommandStart
 
 bot=Bot(token='8363227513:AAHv8gm6oGzoZ5WAD119SLUl_XqHE0ERYPs')
 dp=Dispatcher()
 
-@dp.message(commands=['start'])
+@dp.message(CommandStart())
 async def start_cmd(message:types.Message):
     await message.answer('Hello')
 
+@dp.message()
+async def echo(message:types.Message):
+    await message.answer(f"Hello {message.from_user.id}{message.from_user.last_name}")
+    await message.reply(message.text)
 
 async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
-
+  
 asyncio.run(main())
