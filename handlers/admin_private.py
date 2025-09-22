460cc7daf751555a670750996083b090a145d3af
Alan Nouman <tsoloevalan@gmail.com>
2025-09-22 18:07:07 +0300

    fsm upd

--- handlers/admin_private.py
+++ handlers/admin_private.py
@@ -49,12 +49,21 @@ class AddProduct(StatesGroup):
     price = State()
     image = State()
 
+    texts = {
+        "AddProduct:name": "Введите название товара",
+        "AddProduct:description": "Введите описание товара",
+        "AddProduct:price": "Введите стоимость товара",
+        "AddProduct:image": "Загрузите изображение товара",
+    }
+
 @admin_router.message(StateFilter(None),F.text == "Добавить товар")
 async def add_product(message: types.Message, state: FSMContext):
     await message.answer(
         "Введите название товара", reply_markup=types.ReplyKeyboardRemove()
     )
     await state.set_state(AddProduct.name)
+    print(AddProduct.__all_states__)
+
 
 
 @admin_router.message(StateFilter("*"),Command("отмена"))
@@ -68,10 +77,22 @@ async def cancel_handler(message: types.Message, state: FSMContext) -> None:
     await message.answer("Действия отменены", reply_markup=ADMIN_KB)
 
 
-@admin_router.message(Command("назад"))
-@admin_router.message(F.text.casefold() == "назад")
+@admin_router.message(StateFilter("*"),Command("назад"))
+@admin_router.message(StateFilter("*"),F.text.casefold() == "назад")
 async def cancel_handler(message: types.Message, state: FSMContext) -> None:
-    await message.answer(f"ок, вы вернулись к прошлому шагу")
+    current_state = await state.get_state()
+
+    if current_state == AddProduct.name:
+        await message.answer("Нет прошлого шага")
+        return
+    
+    prev_state = None
+    for step in AddProduct.__all_states__:
+        if step.state == current_state:
+            await state.set_state(prev_state)
+            await message.answer(f"ок, вы вернулись к прошлому шагу \n{AddProduct.texts[prev_state.state]}")
+            return
+        prev_state = step
 
 
 @admin_router.message(AddProduct.name,F.text)
@@ -79,6 +100,12 @@ async def add_name(message: types.Message, state: FSMContext):
     await state.update_data(name=message.text)
     await message.answer("Введите описание товара")
     await state.set_state(AddProduct.description)
+
+@admin_router.message(AddProduct.name)
+async def add_name(message: types.Message, state: FSMContext):
+
+    await message.answer("Пожалуйста, введите текстом название товара")
+
 
 
 
